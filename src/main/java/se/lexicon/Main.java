7d867476ef539ca31ed961fee42dd7575faaa2a6
Attila Bartos <boodney14@gmail.com>
2023-11-15 10:51:30 +0100

    Calculator finished

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,19 +1,69 @@
 package se.lexicon;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Enter two numbers to be calculated!");
+        double number1 = numbers.nextDouble();
+        double number2 = numbers.nextDouble();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Type 1 to Add the Numbers");
+        System.out.println("Type 2 to Subtract the Numbers");
+        System.out.println("Type 3 to Multiply the Numbers");
+        System.out.println("Type 4 to Divide the Numbers");
+
+        int choice = numbers.nextInt();
+
+        double sumOfNumbers = addition(number1,number2);
+        double multipliedNumbers = multiply(number1, number2);
+        double subtractedNumbers = subtraction(number1, number2);
+        double dividedNumbers = division(number1, number2);
+
+
+        //double division = number2 / number1;
+        //double subtraction = number1 - number2;
+        //double multiplication = number1 * number2;
+
+
+        if (choice==1) {
+            System.out.println("The sum of numbers " + number1 + " and " + number2 + " = " + sumOfNumbers);
+        } else if (choice == 2) {
+            System.out.println(number1 + " minus " + number2 + " is " + subtractedNumbers);
+        } else if (choice == 3) {
+            System.out.println(number1 + " times " + number2 + " is " + multipliedNumbers);
+        } else if (choice == 4){
+            System.out.println(number1 + " divided by " + number2 + " = " +dividedNumbers);
+        } else{
+            System.out.println("You need to choose 1 through 4");
         }
+
+
+
+
+
+
+
+
     }
+    static double addition(double num1, double num2){
+        double result = num1 + num2;
+        return result;
+    }
+
+    static double multiply(double num1, double num2){
+        double result = num1 * num2;
+        return result;
+    }
+
+    static double subtraction(double num1, double num2){
+        double result = num1 - num2;
+        return result;
+    }
+
+    static double division(double num1, double num2){
+        double result = num1 / num2;
+        return result;
+    }
+
 }
